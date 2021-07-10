c51e841dc3d704b58e7b6f3b1eee334488545efc
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2021-07-10 10:47:28 +0200

    add 'stappenplan' and written all 3 functions

--- gamelogic.js
+++ gamelogic.js
@@ -1,34 +1,68 @@
+// DOEL: displayWordSoFar
+// word = "javascript" guesses = [];
+// -> "_ _ _ _ _ _ _ _ _ _ "
+// word = "javascript" guesses = ["q", "w", "e", "a", "t"];
+// -> "_ a _ a _ _ _ _ _ t "
+// STRATEGIE:
+// Een string aan elkaar plakken
+// Door over elke letter in het woord heen te loopen
+// Zit die letter er niet in: plak "_ " aan de string
+// Zit die letter er wel in: plak "<letter> " aan de string
+// - [ ] zet het eerste testje op test.only (later weer weghalen)
+// - [ ] loggen: wat is word, wat is guesses
+// - [x] javascript "splitten" word.split("") -> ['j','a','v','a','s','c','r','i','p','t']
+// - [x] Maak een output variable (met let) met de waarde lege string
+// - [x] een loop maken -> we loopen over ['j','a','v','a','s','c','r','i','p','t']
+// - [x] voo elke van letters checken: zit de letter in guesses (guess.includes(letter))?
+// - [x] if else
+// - [x] zo nee: plak "_ " aan de output variable
+// - [x] zo ja: plak "j " aan de output variable (niet j, maar de letter die we dan checken)
+// - [x] klaar? -> onze ouutput string die we hebben gemaakt returnen
+// - [x] Test geslaagd? haal test.only weg en zet het bij de volgende test erbij
+
 function displayWordSoFar(word, guesses) {
-  let dashedWord = "";
-
-  // forloop trough "word" to greate a dashed string
-  for (let i = 0; i < word.length; i++) {
-    let element = word[i];
-    element = "_ ";
-    dashedWord = dashedWord + element;
+  const arrayWord = word.split("");
+  let emptyString = "";
+  for (let i = 0; i < arrayWord.length; i++) {
+    const element = arrayWord[i];
+    if (guesses.includes(element)) {
+      emptyString = emptyString + element + " ";
+    } else {
+      emptyString = emptyString + "_ ";
+    }
   }
-
-  let elementGuesses = "";
-
-  // forloop trough "guesses"
-  for (let i = 0; i < guesses.length; i++) {
-    const element = guesses[i];
-    elementGuesses = elementGuesses + element;
-  }
-
-  console.log(dashedWord);
-  console.log(elementGuesses);
+  return emptyString;
 }
 
-displayWordSoFar("javascript", ["a", "s"]);
+// console.log(displayWordSoFar("javascript", ["a", "s"]));
 
 function isGameWon(word, guesses) {
+  if (!displayWordSoFar(word, guesses).includes("_")) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// console.log(isGameWon("javascript", ["j", "v", "s", "c", "r", "i", "p", "t"]));
+
+function isGameLost(word, guesses) {
+  let counter = 0;
+  const arrayWord = word.split("");
+  for (let i = 0; i < arrayWord.length; i++) {
+    const element = arrayWord[i];
+    if (!guesses.includes(element)) {
+      counter++;
+    }
+  }
+  if (counter > 7) {
+    return true;
+  }
+  return false;
   // WRITE ME
 }
 
-function isGameLost(word, guesses) {
-  // WRITE ME
-}
+console.log(isGameLost("javascript", ["b", "z", "d", "c", "g", "h", "q", "t"]));
 
 module.exports = {
   displayWordSoFar: displayWordSoFar,
