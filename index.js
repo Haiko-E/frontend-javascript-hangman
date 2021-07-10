b992e51bc4158c00ad079149fe7adb065a4571e1
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2021-07-10 14:42:44 +0200

    add if statement when user puts in in 2 or more letters

--- index.js
+++ index.js
@@ -13,15 +13,21 @@ function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
   const letter = question("Raad een letter: ").toLowerCase();
 
+  // Geeft een melding aan de gebruiker wanneer hij meer dan 1 letterinvoert
+  if (letter.length > 1) {
+    console.log("Je mag maar 1 letter invoeren, probeer het opnieuw");
+    return game(word, guesses);
+  }
+
   // voeg de geraden letter toe aan de array met guesses
   guesses.push(letter);
 
-  // laat het huidige woord zien
+  // laat het huidige woord en het galgje zien
 
   console.log(`\n\n Het galgje \n\n ${counter(word, guesses)}\n`);
   console.log(displayWordSoFar(word, guesses), "\n");
 
-  // volgende ronde, wanneer Gamelost en Gamewon beide niet true zijn! we roepen game nog een keer aan.
+  // volgende ronde, wanneer Gamelost en Gamewon beide niet true zijn!, we roepen game nog een keer aan.
   if (isGameWon(word, guesses)) {
     console.log("je hebt GEWONNEN!!! het woord is = " + word.toUpperCase());
     return;
