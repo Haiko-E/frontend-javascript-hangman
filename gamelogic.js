7c0e8b85c7e62a0c06e0afb9526b40993cdf2c4d
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2021-07-10 09:04:50 +0200

    add 2 times for loop in  function displayWordSoFar

--- gamelogic.js
+++ gamelogic.js
@@ -1,6 +1,26 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
+  let dashedWord = "";
+
+  // forloop trough "word" to greate a dashed string
+  for (let i = 0; i < word.length; i++) {
+    let element = word[i];
+    element = "_ ";
+    dashedWord = dashedWord + element;
+  }
+
+  let elementGuesses = "";
+
+  // forloop trough "guesses"
+  for (let i = 0; i < guesses.length; i++) {
+    const element = guesses[i];
+    elementGuesses = elementGuesses + element;
+  }
+
+  console.log(dashedWord);
+  console.log(elementGuesses);
 }
+
+displayWordSoFar("javascript", ["a", "s"]);
 
 function isGameWon(word, guesses) {
   // WRITE ME
