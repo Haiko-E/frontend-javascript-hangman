7a190d57620971d6138da05ff6322b44c69ad117
Haiko Erinkveld <haiko.erinkveld@gmail.com>
2021-07-10 14:26:15 +0200

    add all functions to the file, game galgje works as supposed

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
-const { question } = require("readline-sync");
-const { displayWordSoFar, isGameWon, isGameLost } = require("./gamelogic");
+// importeer een tweetal functies en deze worden opgeslagen in de variabelen die tussen de {} staan
 
+const { question } = require("readline-sync");
+const {
+  counter,
+  displayWordSoFar,
+  isGameWon,
+  isGameLost,
+} = require("./gamelogic");
+
+// de functie wordt hier gedeclareerd maar nog niet uitgevoerd
 function game(word, guesses) {
   console.log("Dit heb je tot nu toe geraden: ", guesses);
-
-  const letter = question("Raad een letter: ");
+  const letter = question("Raad een letter: ").toLowerCase();
 
   // voeg de geraden letter toe aan de array met guesses
   guesses.push(letter);
 
-  // volgende ronde! we roepen game nog een keer aan
-  game(word, guesses);
+  // laat het huidige woord zien
+
+  console.log(`\n\n Het galgje \n\n ${counter(word, guesses)}\n`);
+  console.log(displayWordSoFar(word, guesses), "\n");
+
+  // volgende ronde, wanneer Gamelost en Gamewon beide niet true zijn! we roepen game nog een keer aan.
+  if (isGameWon(word, guesses)) {
+    console.log("je hebt GEWONNEN!!! het woord is = " + word.toUpperCase());
+    return;
+  }
+  if (isGameLost(word, guesses)) {
+    console.log("je hebt VERLOREN het woord was = " + word.toUpperCase());
+    return;
+  } else {
+    game(word, guesses);
+  }
 }
 
 console.log(`
